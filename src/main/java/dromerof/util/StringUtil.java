8cf1a8dec674cd4738aa337c87c060585d9e3e18
Daniel Romero <dandres508@gmail.com>
2024-10-21 14:07:46 -0500

    javatest

---
+++ src/main/java/dromerof/util/StringUtil.java
@@ -0,0 +1,14 @@
+package dromerof.util;
+
+public class StringUtil {
+
+    public static String repeat(String str, int times) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < times; i++) {
+            result.append(str);
+        }
+
+        return result.toString();
+    }
+}
---
+++ src/test/java/dromerof/util/StringUtilTest.java
@@ -0,0 +1,24 @@
+package dromerof.util;
+
+public class StringUtilTest {
+
+    public static void main(String[] args) {
+
+        String result = StringUtil.repeat("Hola", 3);
+        if (result.equals("HolaHolaHola")){
+            System.out.println("OK");
+        }else {
+            System.out.println("ERROR!");
+        }
+
+        String result2 = StringUtil.repeat("Hola", 1);
+        if (!result2.equals("Hola")){
+            System.out.println("OK");
+        } else {
+            System.out.println("ERROR!");
+        }
+
+
+    }
+}
+
