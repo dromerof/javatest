bf807bfd6c082c693fe53bc9994d59dc0481be26
Daniel Romero <dandres508@gmail.com>
2024-10-21 16:08:39 -0500

    test con junit

--- src/test/java/dromerof/util/StringUtilTest.java
+++ src/test/java/dromerof/util/StringUtilTest.java
@@ -1,21 +1,17 @@
 package dromerof.util;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringUtilTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testRepeat() {
 
-        assertEquals(StringUtil.repeat("Hola", 3), "HolaHolaHola");
-
-        assertEquals(StringUtil.repeat("Hola", 1), "Hola");
-
-    }
-
-    private static void assertEquals(String actual, String expected) {
-
-        if (!actual.equals(expected)) {
-            throw new RuntimeException(actual + " is not equal to expected " + expected);
-        }
+        Assert.assertEquals("HolaHolaHola", StringUtil.repeat("Hola", 3));
+        Assert.assertEquals("Hola", StringUtil.repeat("Hola", 1));
 
     }
+
 }
 
