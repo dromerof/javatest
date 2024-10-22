6d788f24872a706836b73b7dd9d70d2cd8adc8f5
Daniel Romero <dandres508@gmail.com>
2024-10-22 12:28:49 -0500

    test de la pasarela de pagos

--- src/main/java/dromerof/payments/PaymentProcessor.java
+++ src/main/java/dromerof/payments/PaymentProcessor.java
@@ -2,13 +2,19 @@ package dromerof.payments;
 
 public class PaymentProcessor {
 
-    private PaymentGateway paymentGateway;
+    private final PaymentGateway paymentGateway;
 
     public PaymentProcessor(PaymentGateway paymentGateway) {
         this.paymentGateway = paymentGateway;
     }
 
-    public boolean makePayment(double amount){
-        return false;
+    public boolean makePayment(double amount) {
+
+        PaymentResponse response = paymentGateway.requestPaymet(new PaymentRequest(amount));
+        if (response.getStatus() == PaymentResponse.PaymentStatus.OK) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
