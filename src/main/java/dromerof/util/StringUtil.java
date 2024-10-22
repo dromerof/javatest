a7f23545b9583b87c955ba6fffe23661e8d61d2d
Daniel Romero <dandres508@gmail.com>
2024-10-22 08:23:14 -0500

    Organizacion de tests con JUnit

--- src/main/java/dromerof/util/StringUtil.java
+++ src/main/java/dromerof/util/StringUtil.java
@@ -3,6 +3,11 @@ package dromerof.util;
 public class StringUtil {
 
     public static String repeat(String str, int times) {
+
+        if (times < 0 ){
+            throw new IllegalArgumentException("negative");
+        }
+
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < times; i++) {
--- src/test/java/dromerof/util/StringUtilTest.java
+++ src/test/java/dromerof/util/StringUtilTest.java
@@ -6,10 +6,27 @@ import org.junit.Test;
 public class StringUtilTest {
 
     @Test
-    public void testRepeat() {
+    public void repeat_string_one() {
 
         Assert.assertEquals("HolaHolaHola", StringUtil.repeat("Hola", 3));
+
+    }
+
+    @Test
+    public void repeat_string_multiple_times() {
         Assert.assertEquals("Hola", StringUtil.repeat("Hola", 1));
+
+    }
+
+    @Test
+    public void repeat_string_zero_times() {
+        Assert.assertEquals("", StringUtil.repeat("Hola", 0));
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void repeat_string_negative_times() {
+        StringUtil.repeat("Hola", -1);
 
     }
 
